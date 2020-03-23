edfc3b54a6c37ee021b9999be1c3493a951235f9
Federico Cassano <gladiatore-romano@virgilio.it>
2020-03-24 00:47:07 +0100

    🥑

--- feedforwardNN/feedforwardNN_ReLU.py
+++ feedforwardNN/feedforwardNN_ReLU.py
@@ -1,29 +1,46 @@
 # ffNN is similar to LogisticRegression but it is able to input non-linear functions
-# As fast as sigmoid to compute but better increases accuracy per iteration
+# ReLU is the best (accuracy to iter rate) because of Hidden Layer
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
 import torchvision.datasets as dsets
 from datetime import datetime
 
+# TODO: Figure out why gpu is slower than cpu
 
 class FeedforwardNeuralNetModel(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(FeedforwardNeuralNetModel, self).__init__()  # super() allows us to inherits everything from nn.module
-        # Linear function
+        # Linear function 1 784 --> 100
         self.fc1 = nn.Linear(input_dim, hidden_dim)
-        # Non-Linearity
-        self.tanh = nn.Tanh()
-        # Linear function again (Readout)
-        self.fc2 = nn.Linear(hidden_dim, output_dim)
+
+        # Non-Linearity 1
+        self.ReLU1 = nn.ReLU()
+
+        # Linear function 2 100 --> 100
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+
+        # Non-Linearity 2
+        self.ReLU2 = nn.ReLU()
+
+        # Linear function 3 (Readout) 100 --> 10
+        self.fc3 = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
         # Linear function
         out = self.fc1(x)
-        # Non-Linearity
-        out = self.tanh(out)
-        # Linear function again (Readout)
+
+        # Non-Linearity 1
+        out = self.ReLU1(out)
+
+        # Linear function 2
         out = self.fc2(out)
+
+        # Non-Linearity 2
+        out = self.ReLU2(out)
+
+        # Linear function 3 (Readout)
+        out = self.fc3(out)
         return out
 
 
